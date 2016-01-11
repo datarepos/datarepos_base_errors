ba529008a74a5e390368c20c08f3479039898485
Bill McKinnon <bill.mckinnon@bmck.org>
2016-01-11 11:06:37 -0600

    bug fix in require statements

--- lib/datarepos_base_errors.rb
+++ lib/datarepos_base_errors.rb
@@ -1,10 +1,10 @@
 # $Id$
 # $(c)$
 
-require_relative 'repo_error/version.rb'
+require_relative 'datarepos/repo_error/version.rb'
 
-require_relative 'repo_error/abstract_method_error.rb'
-require_relative 'repo_error/incompatible_processor_error.rb'
-require_relative 'repo_error/unavailable_uri_error.rb'
-require_relative 'repo_error/unsuccessful_transformation_error.rb'
-require_relative 'repo_error/verification_error.rb'
+require_relative 'datarepos/repo_error/abstract_method_error.rb'
+require_relative 'datarepos/repo_error/incompatible_processor_error.rb'
+require_relative 'datarepos/repo_error/unavailable_uri_error.rb'
+require_relative 'datarepos/repo_error/unsuccessful_transformation_error.rb'
+require_relative 'datarepos/repo_error/verification_error.rb'
